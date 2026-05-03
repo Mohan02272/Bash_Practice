b8c54af28b76d8162bb0aa85f201e21bbbf7cb2e
Rashedul Islam Mohan <rashedul.akash.75@gmail.com>
2026-05-04 01:09:30 +0600

    Learning Bash Script to take input from user

---
+++ practice-day03.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "===== DAY 02 BASH ====="
+
+echo "Enter your name:"
+read name
+
+echo "Enter your age:"
+read age
+
+echo "Hello $name"
+
+echo "You are $age years old"
+
+echo "Current date is: $(date)"
