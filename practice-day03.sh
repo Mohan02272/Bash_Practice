7230a97f91c7afd438bb01129865d0ee3bc732ca
Rashedul Islam Mohan <rashedul.akash.75@gmail.com>
2026-05-04 01:19:22 +0600

    Upgraded my bash script

--- practice-day03.sh
+++ practice-day03.sh
@@ -13,3 +13,9 @@ echo "Hello $name"
 echo "You are $age years old"
 
 echo "Current date is: $(date)"
+
+if [ $age -ge 18 ]; then
+    echo "You are eligible for a job"
+else
+    echo "You are not eligible for a job"
+fi
