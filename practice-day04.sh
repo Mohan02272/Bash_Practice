dab109fe690d004fac273e632002fa989053b0f7
Rashedul Islam Mohan <rashedul.akash.75@gmail.com>
2026-05-05 11:51:30 +0600

    Learning while loop

--- practice-day04.sh
+++ practice-day04.sh
@@ -4,7 +4,7 @@ echo "===== DAY 04 LOOP ====="
 
 echo "Printing numbers from 1 to 5"
 
-for i in {1..5}
+for i in {1..2}
 
 do
     echo "Number: $i"
@@ -14,5 +14,21 @@ do
 
     echo "Disk usage:"
     df -h | head -n 2
+
+done
+
+
+count=1
+
+while [ $count -le 2 ]
+do
+    echo "Loop number: $count"
+    echo "Date:"
+    date
+
+    echo "disk usage:"
+    df -h | head -n 2
+
+    ((count++))
 
 done
