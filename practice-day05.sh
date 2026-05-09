a4715f7bf631f00ef31bf50176cd92a2ddd849f3
Rashedul Islam Mohan <rashedul.akash.75@gmail.com>
2026-05-09 22:58:40 +0600

    Modified Disk usage Script

--- practice-day05.sh
+++ practice-day05.sh
@@ -1,8 +1,13 @@
-
 #!/bin/bash
 
 echo "===== DISK MONITORING SCRIPT ====="
 
-usage=$(df -h / | awk 'NR==2 {print $5}')
+usage=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
 
-echo "Current disk usage: $usage"
+echo "Current disk usage: $usage%"
+
+if [ $usage -ge 80 ]; then
+    echo "WARNING: Disk usage is high!"
+else
+    echo "Disk usage is under control"
+fi
